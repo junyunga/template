bc1d50fd4b0845bf7628e0ce277abe9148c0aa65
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-05-21 13:19:29 +0900

    firebase 연동

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,56 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+{{books}}
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    import Firebase from 'firebase';
+    // Initialize Firebase
+    let config = {
+        apiKey: "AIzaSyCqCcTRwgvbanOdIZWQajDGdbsOkwO0Sww",
+        authDomain: "template-4f9fb.firebaseapp.com",
+        databaseURL: "https://template-4f9fb.firebaseio.com",
+        projectId: "template-4f9fb",
+        storageBucket: "template-4f9fb.appspot.com",
+        messagingSenderId: "122884676533"
+    };
+    let app = Firebase.initializeApp(config);
+    let db = app.database();
+    let booksRef = db.ref('db');
+    booksRef.on("value", function(snapshot) {
+        console.log(snapshot.val());
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+    export default {
+        name: 'App',
+        firebase: function() {
+            return {
+                books: booksRef
+            }
+        },
+        data () {
+            return {
+                newBook: {
+                    title: '11',
+                    author: '11',
+                    url : 'http://'
+                }
+            }
+        },
+        mounted: function() {
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueFire from 'vuefire'
+import VueBlu from 'vue-blu'
+import 'vue-blu/dist/css/vue-blu.min.css'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
-
+Vue.use(VueFire);
+Vue.use(VueBlu);
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router,
+    components: {App},
+    template: '<App/>'
+});
