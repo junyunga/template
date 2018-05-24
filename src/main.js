424310eef1e5b06f1f95f9b2951c92990feb612d
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-05-24 11:44:24 +0900

    vuetify add

--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ import 'vue-blu/dist/css/vue-blu.min.css'
 import App from './App'
 import router from './router'
 import {store} from './store/store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify);
 Vue.use(VueFire);
 Vue.use(VueBlu);
 /* eslint-disable no-new */
