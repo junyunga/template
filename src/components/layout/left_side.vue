fd11b404499fcd1fc3324ccb1f7c75f42f4629cf
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-05-24 09:18:13 +0900

    ?

---
+++ src/components/layout/left_side.vue
@@ -0,0 +1,10 @@
+<template>
+    <aside>
+        left
+    </aside>
+</template>
+<script>
+    export default {
+        name: 'vue_left_side'
+    }
+</script>
---
+++ src/main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <vue_header></vue_header>
+        <vue_left_side></vue_left_side>
+    </div>
+</template>
+<script>
+    import vue_header from './components/layout/vue_header';
+    import vue_left_side from './components/layout/left_side';
+
+    export default {
+        name: 'MainVue',
+        components: {
+            vue_header,
+            vue_left_side
+        }
+    }
+</script>
